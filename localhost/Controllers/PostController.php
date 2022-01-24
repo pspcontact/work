1a5927f7b905542c406c44b1ea17f731f26419e2
pspcontact <77489418+pspcontact@users.noreply.github.com>
2022-01-24 12:44:24 +0700

    Update PostController.php

--- localhost/Controllers/PostController.php
+++ localhost/Controllers/PostController.php
@@ -1,25 +1,33 @@
 <?php
-//header('Location: ../Views/index.php');
-if(isset($_POST['data']) && $_POST['1'] == true) {
-    $data = strtok($_POST['data'], " ");
-    while($data)
+if(isset($_POST['data'])) 
+{
+    function getData()
     {
-	    echo $data."<br>"; 
-	    $data = strtok(" ");
+        if($_POST['strtok'] == true) {
+            $data = strtok($_POST['data'], " ");
+            while($data)
+            {
+                echo $data."<br>"; 
+                $data = strtok(" ");
+            }
+        } elseif($_POST['foreach'] == true) {
+            $data = explode(" ",$_POST['data']);
+            foreach($data as $d)
+            {
+                echo $d."<br />";
+            }
+        
+        } elseif($_POST['for'] == true) {
+            $data = explode(" ", $_POST['data']);
+            for($d = 0; $d < count($data); $d++ )
+            {
+                echo $data[$d]."<br />";
+            }
+        
     }
-} elseif(isset($_POST['data']) && $_POST['2'] == true) {
-    $data = explode(" ",$_POST['data']);
-    foreach($data as $d)
-    {
-        echo $d."<br />";
-    }
-    
-} elseif(isset($_POST['data']) && $_POST['3'] == true) {
-    $data = explode(" ", $_POST['data']);
-    for($d = 0; $d < count($data); $d )
-    {
-        echo $data[$d]."<br />";
-    }
-    
 }
-
+?>
+<!-- <script> window.location.href="http://testpsr.local/Views/"; </script> -->
+<?php
+getData();
+}
