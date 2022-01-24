712bc8bbacf4517ecadd38d58a054ee914f3aa93
pspcontact <77489418+pspcontact@users.noreply.github.com>
2022-01-24 12:44:47 +0700

    Update index.php

--- localhost/Views/index.php
+++ localhost/Views/index.php
@@ -4,20 +4,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <title>TestPsr</title>
 </head>
 <body>
-    <form action="../Controllers/PostController.php" method="post">
+    <form action="../Controllers/PostController.php" method="POST">
         <h3>Напарсили</h3>
-        <textarea name="data"  cols="50" rows="30"></textarea>
-        <p><input type="checkbox" name="1" value="1">1</p>
-        <p><input type="checkbox" name="2" value="2">2</p>
-        <p><input type="checkbox" name="3" value="3">3</p>
-        <button>Отправить</button>
+        <textarea name="data" id="" cols="50" rows="30"></textarea>
+        <h3>Укажите метод парсинга</h3>
+        <p><input type="checkbox" name="strtok" value="strtok" > strtok + while</p>
+        <p><input type="checkbox" name="foreach" value="foreach" > explode + foreach</p>
+        <p><input type="checkbox" name="for" value="for" > explode + for</p>
+        <button>Выбрать</button>
     </form>
-<?php
-require '../Models/Post.php';
-
-?>
+    <?php 
+    require "../Controllers/PostController.php"; 
+    getData();
+    ?>
 </body>
 </html>
